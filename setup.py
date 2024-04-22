c25843cd65bccb8f66ca222f8da52ccc37dd6b48
satyadeep-ih <74664078+satyadeep-ih@users.noreply.github.com>
2024-04-22 09:13:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='abdm_fhir_converter',
-    packages=find_packages(include=['fhir_converter']),
+    packages=find_packages(include=['abdm_fhir_converter']),
     version='0.0.5',
     description='Intelehealth fhir converter',
     install_requires=["fhir.resources"],
