8914d942c97da6f8db0075eac593d44aafb9fd1f
satyadeep-ih <74664078+satyadeep-ih@users.noreply.github.com>
2024-04-22 08:45:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='fhir_converter',
     packages=find_packages(include=['fhir_converter']),
     version='0.0.5',
-    description='Dashmed fhir converter',
+    description='Intelehealth fhir converter',
     install_requires=["fhir.resources"],
-    author='Vibhor',
+    author='Satyadeep',
 )
